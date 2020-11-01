adc65b4bcdf31a427572dcb2f7ebb89520d33b97
DAKXIM <71939850+DAKXIM@users.noreply.github.com>
2020-11-01 14:12:48 -0600

    gatsby-starter-wordpress

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  transform: {
+    "^.+\\.jsx?$": `<rootDir>/jest-preprocess.js`,
+  },
+  // moduleNameMapper: {
+  //   ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
+  //   ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/__mocks__/file-mock.js`,
+  // },
+  testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
+  transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
+  globals: {
+    __PATH_PREFIX__: ``,
+  },
+  testURL: `http://localhost`,
+  setupFiles: [`<rootDir>/loadershim.js`],
+  globalSetup: `./test-site/test-utils/global-setup-jest.js`,
+  globalTeardown: `./test-site/test-utils/global-teardown-jest.js`,
+  setupFilesAfterEnv: [`./test-site/test-utils/jest.setup.js`],
+}
